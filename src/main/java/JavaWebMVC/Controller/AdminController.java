e49d822539eb989a166dca8ee548126cc3b56643
Yuhtaro <85222053+nameishuy@users.noreply.github.com>
2022-03-15 12:16:57 +0700

    Design NotAdmin Page
    Design NotAdmin Page & Fix check if user didn't login and go to Admin Page

--- src/main/java/JavaWebMVC/Controller/AdminController.java
+++ src/main/java/JavaWebMVC/Controller/AdminController.java
@@ -21,93 +21,94 @@ public class AdminController {
 	
 	@RequestMapping(value = {"/admin/index"})
 	public String Admin(HttpSession session) {
-		if(session.getAttribute("Role").toString() == "true") return "/admin/index";
-		else return "/admin/NoAdmin";
-
+			if(session.equals("Role") && (session.getAttribute("Role").toString() == "true")) return "/admin/index";
+			else return "/admin/NoAdmin";
 	}
 	
 	@RequestMapping(value= {"/admin/account-manager"})
 	public ModelAndView Acc_Manager(HttpSession session) throws UnsupportedEncodingException {
-		if(session.getAttribute("Role").toString() == "true") {
-			Boolean role = false;
-			String linkapi = "https://bookingapiiiii.herokuapp.com/khachhangforadmin/" + role;
-			String resAPI = JavaWebMVC.API.CallAPI.Get(linkapi).toString();
-			int length=0;
-			
-			ArrayList<User> listUser = new ArrayList<User>();
-			if(resAPI != null) {
-				JSONArray json = new JSONArray(JavaWebMVC.API.CallAPI.Get(linkapi).toString());
-				json.forEach(data -> {
-					JSONObject jsonobject = (JSONObject) data;
-					User user = new User();
-					user.setId(jsonobject.getString("_id"));
-					user.setHoTen(jsonobject.getString("HoTen"));
-					if (jsonobject.has("Email")) user.setEmail(jsonobject.getString("Email"));	    	
-					user.setTaikhoan(jsonobject.getString("Taikhoan"));
-					user.setRole(jsonobject.getBoolean("Role"));
-					listUser.add(user);
-				});
-				length = json.length();
+			if(session.equals("Role") && (session.getAttribute("Role").toString() == "true")) {
+				Boolean role = false;
+				String linkapi = "https://bookingapiiiii.herokuapp.com/khachhangforadmin/" + role;
+				String resAPI = JavaWebMVC.API.CallAPI.Get(linkapi).toString();
+				int length=0;
+				
+				ArrayList<User> listUser = new ArrayList<User>();
+				if(resAPI != null) {
+					JSONArray json = new JSONArray(JavaWebMVC.API.CallAPI.Get(linkapi).toString());
+					json.forEach(data -> {
+						JSONObject jsonobject = (JSONObject) data;
+						User user = new User();
+						user.setId(jsonobject.getString("_id"));
+						user.setHoTen(jsonobject.getString("HoTen"));
+						if (jsonobject.has("Email")) user.setEmail(jsonobject.getString("Email"));	    	
+						user.setTaikhoan(jsonobject.getString("Taikhoan"));
+						user.setRole(jsonobject.getBoolean("Role"));
+						listUser.add(user);
+					});
+					length = json.length();
+				}
+				
+				ModelAndView mv = new ModelAndView("/admin/account-manager");
+				
+				mv.addObject("listUser",listUser);
+				mv.addObject("length", length);
+				return mv;
 			}
-			
-			ModelAndView mv = new ModelAndView("/admin/account-manager");
-			
-			mv.addObject("listUser",listUser);
-			mv.addObject("length", length);
-			return mv;
-		}
-		else{
-			ModelAndView mv = new ModelAndView("/admin/NoAdmin");
-			return mv;
-		}
+			else{
+				ModelAndView mv = new ModelAndView("/admin/NoAdmin");
+				return mv;
+			}
 		
 	}
 	
 	@RequestMapping(value = {"/admin/bill-pay"})
 	public ModelAndView Bill_Pay(HttpSession session) {
-		if(session.getAttribute("Role").toString() == "true") {
-			String linkapi = "https://bookingapiiiii.herokuapp.com/DonHang/";
-			String resAPI = JavaWebMVC.API.CallAPI.Get(linkapi).toString();
-			
-			ArrayList<Bill> listBill = new ArrayList<Bill>();
-			if(resAPI != null) {
-				JSONArray json = new JSONArray(JavaWebMVC.API.CallAPI.Get(linkapi).toString());
-				json.forEach(data -> {
-					JSONObject jsonobject = (JSONObject) data;
-					Bill bill = new Bill();
-					bill.setDate(jsonobject.getString("Ngaydat"));
-					bill.setId(jsonobject.getString("id"));
-					bill.setUsername(jsonobject.getString("HoTen"));  	
-					listBill.add(bill);
-				});
+			if(session.equals("Role") && (session.getAttribute("Role").toString() == "true")) {
+				String linkapi = "https://bookingapiiiii.herokuapp.com/DonHang/";
+				String resAPI = JavaWebMVC.API.CallAPI.Get(linkapi).toString();
+				
+				ArrayList<Bill> listBill = new ArrayList<Bill>();
+				if(resAPI != null) {
+					JSONArray json = new JSONArray(JavaWebMVC.API.CallAPI.Get(linkapi).toString());
+					json.forEach(data -> {
+						JSONObject jsonobject = (JSONObject) data;
+						Bill bill = new Bill();
+						bill.setDate(jsonobject.getString("Ngaydat"));
+						bill.setId(jsonobject.getString("id"));
+						bill.setUsername(jsonobject.getString("HoTen"));  	
+						listBill.add(bill);
+					});
+				}
+				ModelAndView mv = new ModelAndView("/admin/billpay");
+				mv.addObject("listBill",listBill);
+				return mv;
+			}else {
+				ModelAndView mv = new ModelAndView("/admin/NoAdmin");
+				return mv;
 			}
-			ModelAndView mv = new ModelAndView("/admin/billpay");
-			mv.addObject("listBill",listBill);
-			return mv;
-		}else {
-			ModelAndView mv = new ModelAndView("/admin/NoAdmin");
-			return mv;
-		}
+		
 
 	}
 	
 	@RequestMapping(value = {"/admin/storage-products"})
 	public ModelAndView Storage(HttpSession session) {
-		if(session.getAttribute("Role").toString() == "true") {
-			//Code more here:
-			
-			
-			ModelAndView mv = new ModelAndView("/admin/storage");
-			return mv;
-		}else {
-			ModelAndView mv = new ModelAndView("/admin/NoAdmin");
-			return mv;
-		}
+			if(session.equals("Role") && (session.getAttribute("Role").toString() == "true")) {
+				//Code more here:
+				
+				
+				ModelAndView mv = new ModelAndView("/admin/storage");
+				return mv;
+			}else {
+				ModelAndView mv = new ModelAndView("/admin/NoAdmin");
+				return mv;
+			}
+
 	}
 	
 	@RequestMapping(value = {"/admin/setting"})
 	public ModelAndView Setting(HttpSession session) {
-		if(session.getAttribute("Role").toString() == "true") {
+		if(session.equals("Role") && (session.getAttribute("Role").toString() == "true")) {
 			//Code more here:
 			
 			
--- src/main/webapp/WEB-INF/views/admin/NoAdmin.jsp
+++ src/main/webapp/WEB-INF/views/admin/NoAdmin.jsp
@@ -2,5 +2,64 @@
     pageEncoding="UTF-8"%>
 
 <body>
-	<h1>Bạn Không Phải Admin</h1>
+	<style>
+		/* NotAdmin's Page*/
+		.NotAdminPage__Container{
+			min-height: 70vh;
+			display: flex;
+			flex-flow: column nowrap;
+			margin: 0 auto;
+			justify-content: center;
+			align-items: center;
+		}
+		.NotAdminPage__Container-Image{
+			width: 300px;
+			height: 300px;
+		}
+		.NotAdminPage__Container-Image img{
+			width: 100%;
+			height: 100%;
+		}
+		.NotAdminPage__Container-Announce{
+			text-align: center;
+			height: fit-content;
+		}
+		.NotAdminPage__Container-Announce-Content{
+			margin-bottom: 20px;
+			font-weight: 600;
+			font-size: 23px;
+			color: red;
+		}
+		.NotAdminPage__Container-Announce-BtnBack{
+			background-color: rgba(255, 0, 0, 0.651);
+			margin-top: 20px;
+			padding: 10px;
+			border-radius: .5rem;
+			cursor: pointer;
+			text-decoration: none;
+			transition: 1s;
+			color: white;
+		}
+		
+		.NotAdminPage__Container-Announce-BtnBack:hover{
+			box-shadow: 0 0 10px 0 red;
+			color: white;
+			background-color: red;
+			text-decoration: none;
+		}
+	</style>
+    <div class="NotAdminPage__Container">
+        <div class="NotAdminPage__Container-Image">
+            <img src="https://c.tenor.com/aiyNkzmKPwEAAAAC/meong_cat.gif" alt="">
+        </div>
+        <div class="NotAdminPage__Container-Announce">
+            <div class="NotAdminPage__Container-Announce-Content">
+                Bạn Không Thể Truy Cập! <br>
+                Vui Lòng Quay Về Trang Chủ
+            </div>
+            <a class="NotAdminPage__Container-Announce-BtnBack" href="/">
+                Trang Chủ
+            </a>
+        </div>
+    </div>
 </body>
